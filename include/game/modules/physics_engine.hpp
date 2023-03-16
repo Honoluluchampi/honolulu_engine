74122a48b6ab429c065ad219571a01e375098400
Honoluluchampi <msd13aj@gmail.com>
2023-03-16 18:24:04 +0900

    add command buffer for physics engine

--- include/game/modules/physics_engine.hpp
+++ include/game/modules/physics_engine.hpp
@@ -1,13 +1,39 @@
 #pragma once
 
-namespace hnll::physics {
+// std
+#include <vector>
+
+// lib
+#include <vulkan/vulkan.h>
+
+namespace hnll {
+
+namespace graphics { class device; }
+
+namespace game {
 
 class physics_engine
 {
   public:
-    void submit_async_work();
-  private:
+    explicit physics_engine(graphics::device& device);
+    ~physics_engine();
 
+    void submit_async_work();
+
+  private:
+    VkCommandBuffer get_current_command_buffer()
+    { return command_buffers_[current_frame_index_]; }
+
+    void create_command_buffers();
+    void free_command_buffers();
+
+    graphics::device& device_;
+
+    std::vector<VkCommandBuffer> command_buffers_;
+    int current_frame_index_ = 0;
+    bool is_frame_started_ = false;
+
+    int current_semaphore_value_ = 0;
 };
 
-} // namespace hnll::physics
+}} // namespace hnll::physics
--- modules/game/modules/physics_engine.cpp
+++ modules/game/modules/physics_engine.cpp
@@ -1,6 +1,50 @@
 // hnll
 #include <game/modules/physics_engine.hpp>
+#include <graphics/device.hpp>
+#include <graphics/timeline_semaphore.hpp>
+#include <graphics/swap_chain.hpp>
 
 namespace hnll::game {
+
+physics_engine::physics_engine(graphics::device &device) : device_(device)
+{
+  create_command_buffers();
+}
+
+physics_engine::~physics_engine()
+{
+  free_command_buffers();
+}
+
+void physics_engine::submit_async_work()
+{
+
+}
+
+void physics_engine::create_command_buffers()
+{
+  command_buffers_.resize(graphics::swap_chain::MAX_FRAMES_IN_FLIGHT);
+
+  // specify command pool and number of buffers to allocate
+  VkCommandBufferAllocateInfo alloc_info{};
+  alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  // if the allocated command buffers are primary or secondary command buffers
+  alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  alloc_info.commandPool = device_.get_command_pool();
+  alloc_info.commandBufferCount = static_cast<uint32_t>(command_buffers_.size());
+
+  if (vkAllocateCommandBuffers(device_.get_device(), &alloc_info, command_buffers_.data()) != VK_SUCCESS)
+    throw std::runtime_error("failed to allocate command buffers!");
+}
+
+void physics_engine::free_command_buffers()
+{
+  vkFreeCommandBuffers(
+    device_.get_device(),
+    device_.get_command_pool(),
+    static_cast<float>(command_buffers_.size()),
+    command_buffers_.data());
+  command_buffers_.clear();
+}
 
 } // namespace hnll::game
