e7737bbf1cb1851c4e238bbb80350917bcfe4cfb
Honoluluchampi <msd13aj@gmail.com>
2023-03-17 00:40:06 +0900

    create compute pipeline

--- include/game/compute_shader.hpp
+++ include/game/compute_shader.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <vulkan/vulkan.h>
+// hnll
 #include <graphics/device.hpp>
+#include <utils/utils.hpp>
+
+// lib
+#include <vulkan/vulkan.h>
 
 namespace hnll::game {
 
@@ -11,14 +15,18 @@ class compute_shader
   public:
     compute_shader(graphics::device& device) : device_(device)
     { static_cast<Derived>(this)->setup(); }
+
   protected:
     // write this function for each compute shader
     void setup();
-    void create_pipeline_layout(const std::vector<VkDescriptorSetLayout>& desc_set_layouts);
-    void create_pipeline();
+    void create_pipeline(
+      const std::string& filepath,
+      const std::vector<VkDescriptorSetLayout>& desc_set_layouts
+    );
 
-    VkPipeline pipeline_;
+    VkShaderModule shader_module_;
     VkPipelineLayout pipeline_layout_;
+    VkPipeline pipeline_;
 
     graphics::device& device_;
 };
@@ -26,8 +34,21 @@ class compute_shader
 #define CS_API template <typename Derived>
 #define CS_TYPE compute_shader<Derived>
 
-CS_API void CS_TYPE::create_pipeline_layout(const std::vector<VkDescriptorSetLayout>& desc_set_layouts)
+CS_API void CS_TYPE::create_pipeline(
+  const std::string &filepath,
+  const std::vector<VkDescriptorSetLayout> &desc_set_layouts)
 {
+  // create shader module
+  auto raw_code = utils::read_file_for_shader(filepath);
+  shader_module_ = device_.create_shader_module(raw_code);
+
+  VkPipelineShaderStageCreateInfo shader_stage_info{};
+  shader_stage_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+  shader_stage_info.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+  shader_stage_info.module = shader_module_;
+  shader_stage_info.pName = "main";
+
+  // create pipeline layout
   VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
   pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
   pipelineLayoutInfo.setLayoutCount = desc_set_layouts.size();
@@ -36,17 +57,11 @@ CS_API void CS_TYPE::create_pipeline_layout(const std::vector<VkDescriptorSetLay
   if (vkCreatePipelineLayout(device_.get_device(), &pipelineLayoutInfo, nullptr, &pipeline_layout_) != VK_SUCCESS) {
     throw std::runtime_error("failed to create compute pipeline layout!");
   }
-}
-
-CS_API void CS_TYPE::create_pipeline()
-{
-  // create shader module
-  auto shader_module = device_.create_shader_module()
 
   VkComputePipelineCreateInfo create_info {};
   create_info.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
   create_info.layout = pipeline_layout_;
-  create_info.stage = computeShaderStageInfo;
+  create_info.stage = shader_stage_info;
 
   if (vkCreateComputePipelines(device_.get_device(), VK_NULL_HANDLE, 1, &create_info, nullptr, &pipeline_) != VK_SUCCESS) {
     throw std::runtime_error("failed to create compute pipeline!");
