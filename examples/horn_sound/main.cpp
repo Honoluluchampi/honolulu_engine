33457bef54c29341b21bf6df202bb4c094528af0
HonoluluChampi <msd13aj@gmail.com>
2023-05-18 15:28:32 +0900

    horn shading

--- examples/horn_sound/main.cpp
+++ examples/horn_sound/main.cpp
@@ -9,10 +9,7 @@ namespace hnll {
 DEFINE_PURE_ACTOR(horn)
 {
   public:
-    horn() : game::pure_actor_base<horn>()
-    {
-
-    }
+    horn() : game::pure_actor_base<horn>() {}
 
     void bind(VkCommandBuffer command) {}
     void draw(VkCommandBuffer command) { vkCmdDraw(command, 6, 1, 0, 0); }
@@ -21,20 +18,62 @@ DEFINE_PURE_ACTOR(horn)
     uint32_t            get_rc_id() const { return id_; }
     utils::shading_type get_shading_type() const { return utils::shading_type::UNIQUE; }
 
-  private:
-    float length;
-    float width;
-    uint32_t id_;
+    float length = 100.f;
+    float width = 20.f;
+    uint32_t id;
 };
+
+// include push constant (shared with the shader)
+#include "common.h"
 
 DEFINE_SHADING_SYSTEM(horn_shading_system, horn)
 {
   public:
     DEFAULT_SHADING_SYSTEM_CTOR(horn_shading_system, horn);
 
-    void setup() {}
-    void render(const utils::graphics_frame_info& info) {}
+    void setup()
+    {
+      pipeline_layout_ = create_pipeline_layout<horn_push>(
+        static_cast<VkShaderStageFlagBits>(VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT),
+        {}
+      );
+      pipeline_ = create_pipeline(
+        pipeline_layout_,
+        game::graphics_engine_core::get_default_render_pass(),
+        "/examples/horn_sound/shaders/spv/",
+        { "horn.vert.spv", "horn.frag.spv" },
+        { VK_SHADER_STAGE_VERTEX_BIT, VK_SHADER_STAGE_FRAGMENT_BIT },
+        graphics::pipeline::default_pipeline_config_info()
+      );
+    }
+
+    void render(const utils::graphics_frame_info& info)
+    {
+      if (target_ != nullptr) {
+        auto command = info.command_buffer;
+        set_current_command_buffer(command);
+        bind_pipeline();
+
+        // prepare push constants
+        auto window_size = game::graphics_engine_core::get_window_size();
+        horn_push push;
+        push.h_length = target_->length;
+        push.h_width  = target_->width;
+        push.w_width  = std::get<0>(window_size);
+        push.w_height = std::get<1>(window_size);
+        bind_push(push, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT);
+
+        target_->draw(command);
+      }
+    }
+
+    static void set_target(u_ptr<horn>&& target) { target_ = std::move(target); }
+
+  private:
+    static u_ptr<horn> target_;
 };
+
+u_ptr<horn> horn_shading_system::target_;
 
 SELECT_ACTOR(horn);
 SELECT_SHADING_SYSTEM(horn_shading_system);
@@ -42,14 +81,16 @@ SELECT_COMPUTE_SHADER();
 
 DEFINE_ENGINE(horn_sound)
 {
-
+  public:
+    explicit ENGINE_CTOR(horn_sound)
+    { horn_shading_system::set_target(std::make_unique<horn>()); }
 };
 
 } // namespace hnll
 
 int main()
 {
-  hnll::horn_sound engine;
+  hnll::horn_sound engine("horn_sound");
   try { engine.run(); }
   catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
--- examples/horn_sound/shaders/horn.frag
+++ examples/horn_sound/shaders/horn.frag
@@ -1,13 +1,10 @@
 #version 450
 
-layout (location = 0) out vec4 out_color;
+#extension GL_GOOGLE_include_directive : require
 
-struct horn_push {
-  float h_length; // horn
-  float h_width;
-  float w_width; // window
-  float w_height;
-};
+#include "../common.h"
+
+layout (location = 0) out vec4 out_color;
 
 layout(push_constant) uniform Push { horn_push push; };
 
@@ -15,7 +12,9 @@ const float line_width = 3.f;
 
 void main() {
   bool on_length = abs(gl_FragCoord.x - push.w_width / 2.f) < push.h_length / 2.f;
-  bool on_width  = abs(gl_FragCoord.y - push.w_height / 2.f) < line_width / 2.f;
+  bool on_width  = abs(gl_FragCoord.y - push.w_height / 2.f) < push.h_width / 2.f + line_width
+                && abs(gl_FragCoord.y - push.w_height / 2.f) > push.h_width /2.f;
 
-  out_color = vec4(vec3(1.f, 1.f, 1.f) * on_length * on_width, 1.f);
+  float val = float(on_length) * float(on_width) * 1.f;
+  out_color = vec4(vec3(val, val, val), 1.f);
 }
