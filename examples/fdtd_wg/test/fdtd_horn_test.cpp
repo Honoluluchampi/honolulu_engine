5ea79adcebe59c34d1275f5226574b12b5ae1e0c
HonoluluChampi <msd13aj@gmail.com>
2023-07-11 18:04:20 +0900

    fix test

--- examples/fdtd_wg/test/fdtd_horn_test.cpp
+++ examples/fdtd_wg/test/fdtd_horn_test.cpp
@@ -19,7 +19,7 @@ TEST(fdtd_horn, ctor) {
     340.f,
     6,
     { 2, 1, 2 },
-    { { 0.1f, 0.2f }, { 0.30000001f, 0.4f }, { 0.2f, 0.2f } });
+    { { 0.1f, 0.2f }, { 0.3f, 0.4f }, { 0.2f, 0.2f } });
 
   // physical constants
   EXPECT_EQ(0.01f, horn->get_dt());
@@ -37,13 +37,13 @@ TEST(fdtd_horn, ctor) {
   EXPECT_EQ(1, dim[1]);
   EXPECT_EQ(2, dim[2]);
   EXPECT_NEQ(0.1f, size_infos[0].x());
-  EXPECT_NEQ(0.2f, size_infos[0].y());
+  EXPECT_NEQ(0.18f, size_infos[0].y());
   EXPECT_NEQ(2.f,  size_infos[0].w());
   EXPECT_NEQ(0.3f, size_infos[1].x());
-  EXPECT_NEQ(0.4f, size_infos[1].y());
+  EXPECT_NEQ(0.38f, size_infos[1].y());
   EXPECT_NEQ(1.f,  size_infos[1].w());
   EXPECT_NEQ(0.2f, size_infos[2].x());
-  EXPECT_NEQ(0.2f, size_infos[2].y());
+  EXPECT_NEQ(0.18f, size_infos[2].y());
   EXPECT_NEQ(2.f,  size_infos[2].w());
 
   auto edge_infos = horn->get_edge_infos();
@@ -54,18 +54,18 @@ TEST(fdtd_horn, ctor) {
   EXPECT_NEQ(0.6f, horn->get_x_max());
 
   EXPECT_EQ(0, edge_infos[0].y());
-  EXPECT_EQ(50, edge_infos[1].y());
-  EXPECT_EQ(65, edge_infos[2].y());
-  EXPECT_EQ(549, edge_infos[3].y());
-  EXPECT_EQ(549, horn->get_whole_grid_count());
+  EXPECT_EQ(45, edge_infos[1].y());
+  EXPECT_EQ(60, edge_infos[2].y());
+  EXPECT_EQ(522, edge_infos[3].y());
+  EXPECT_EQ(522, horn->get_whole_grid_count());
 
   auto grid_counts = horn->get_grid_counts();
   EXPECT_EQ(5,  grid_counts[0].x());
-  EXPECT_EQ(10, grid_counts[0].y());
+  EXPECT_EQ(9, grid_counts[0].y());
   EXPECT_EQ(15, grid_counts[1].x());
   EXPECT_EQ(1,  grid_counts[1].y());
   EXPECT_EQ(22, grid_counts[2].x());
-  EXPECT_EQ(22, grid_counts[2].y());
+  EXPECT_EQ(21, grid_counts[2].y());
 }
 
 TEST(fdtd_horn, pml_marking) {
