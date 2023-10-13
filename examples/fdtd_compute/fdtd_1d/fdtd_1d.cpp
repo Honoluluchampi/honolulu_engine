24952b1a5e65a4bcd76b0553b2b9f27cd1e6c483
HonoluluChampi <msd13aj@gmail.com>
2023-10-13 19:24:06 +0900

    fix cone

--- examples/fdtd_compute/fdtd_1d/fdtd_1d.cpp
+++ examples/fdtd_compute/fdtd_1d/fdtd_1d.cpp
@@ -47,13 +47,13 @@ float calc_y(float x)
   // straight
 //  return input_edge_width;
   // sine + exponential
-  return input_edge_width +
-    0.003f * (1 - std::cos(std::clamp(90.f * (x - MOUTHPIECE_LENGTH), 0.f, float(4.f * M_PI))))
-    - 0.001f + 0.001f * std::exp(65.f * std::max(x - 0.3f, 0.f));
+//  return input_edge_width +
+//    0.003f * (1 - std::cos(std::clamp(90.f * (x - MOUTHPIECE_LENGTH), 0.f, float(4.f * M_PI))))
+//    - 0.001f + 0.001f * std::exp(65.f * std::max(x - 0.3f, 0.f));
   // stairs
 //  return input_edge_width + 0.003f * (x > 0.15f) + 0.003f * (x > 0.30f);
   // cone
-//  return input_edge_width + 0.07f * std::max(x - MOUTHPIECE_LENGTH, 0.f);
+  return input_edge_width + 0.05f * std::max(x - MOUTHPIECE_LENGTH, 0.f) + 0.001f * (std::exp(50.f * std::max(x - 0.26f, 0.f) - 1.f));
   // exponential
   return input_edge_width - 0.001f + 0.001f * std::exp(65.f * std::max(x - 0.25f, 0.f));
 }
