f4d2a22953ce0db8cde11ebd1391a62703033bbe
HonoluluChampi <msd13aj@gmail.com>
2023-11-01 14:53:11 +0900

    all patterns

--- test/utils/thread_pool_test.cpp
+++ test/utils/thread_pool_test.cpp
@@ -61,19 +61,142 @@ TEST(mt_queue, push_tail_pop_front)
   }
 }
 
-TEST(mt_queue, push_front_pop_back)
+TEST(mt_queue, push_front_pop_front)
 {
   std::vector<int> input = { 1, 7, 2, 3, 7, 5, 0, 4 };
   utils::mt_deque<int> queue;
 
-  for (auto& val : input) {
-    queue.push_front(std::move(val));
+  // push values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue]() {
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          int k = j;
+          queue.push_front(std::move(k));
+        }
+      });
+    }
+
+    JOIN_THREADS(threads);
   }
 
-  for (int i = 0; i < input.size(); i++) {
-    EXPECT_EQ(input[i], *queue.try_pop_back());
+  // pop values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    std::vector<int> anss(THREAD_COUNT);
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue, &anss, i]() {
+        int ans = 0;
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          ans += *queue.try_pop_front();
+        }
+        anss[i] = ans;
+      });
+    }
+
+    JOIN_THREADS(threads);
+
+    int sum = 0;
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      sum += anss[i];
+    }
+
+    EXPECT_EQ(sum, (ELEMENT_COUNT - 1) * ELEMENT_COUNT / 2 * THREAD_COUNT);
   }
-  EXPECT_TRUE(queue.empty());
+}
+
+TEST(mt_queue, push_tail_pop_tail)
+{
+  std::vector<int> input = { 1, 7, 2, 3, 7, 5, 0, 4 };
+  utils::mt_deque<int> queue;
+
+  // push values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue]() {
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          int k = j;
+          queue.push_tail(std::move(k));
+        }
+      });
+    }
+
+    JOIN_THREADS(threads);
+  }
+
+  // pop values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    std::vector<int> anss(THREAD_COUNT);
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue, &anss, i]() {
+        int ans = 0;
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          ans += *queue.try_pop_back();
+        }
+        anss[i] = ans;
+      });
+    }
+
+    JOIN_THREADS(threads);
+
+    int sum = 0;
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      sum += anss[i];
+    }
+
+    EXPECT_EQ(sum, (ELEMENT_COUNT - 1) * ELEMENT_COUNT / 2 * THREAD_COUNT);
+  }
+}
+
+TEST(mt_queue, push_front_pop_tail)
+{
+  std::vector<int> input = { 1, 7, 2, 3, 7, 5, 0, 4 };
+  utils::mt_deque<int> queue;
+
+  // push values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue]() {
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          int k = j;
+          queue.push_front(std::move(k));
+        }
+      });
+    }
+
+    JOIN_THREADS(threads);
+  }
+
+  // pop values
+  {
+    std::vector<std::thread> threads(THREAD_COUNT);
+    std::vector<int> anss(THREAD_COUNT);
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      threads[i] = std::thread([&queue, &anss, i]() {
+        int ans = 0;
+        for (int j = 0; j < ELEMENT_COUNT; j++) {
+          ans += *queue.try_pop_back();
+        }
+        anss[i] = ans;
+      });
+    }
+
+    JOIN_THREADS(threads);
+
+    int sum = 0;
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      sum += anss[i];
+    }
+
+    EXPECT_EQ(sum, (ELEMENT_COUNT - 1) * ELEMENT_COUNT / 2 * THREAD_COUNT);
+  }
 }
 
 } // namespace hnll
